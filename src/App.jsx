c385032eae27823f57b64d487f47d93212ccf455
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-28 20:13:01 +0800

    created app main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Chat } from "stream-chat-react";
+import { StreamChat } from "stream-chat";
+import { ChannelContainer, ChannelListContainer } from "./components";
+import "./App.css";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+const client = StreamChat.getInstance(API_KEY);
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer></ChannelListContainer>
+        <ChannelContainer></ChannelContainer>
+      </Chat>
+    </div>
+  );
 };
 
 export default App;
