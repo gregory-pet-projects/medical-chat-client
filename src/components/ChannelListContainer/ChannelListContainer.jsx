46f19418adcab0ca7d81f3531d75978b64c3a5e7
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-28 20:25:36 +0800

    added header

--- src/components/ChannelListContainer/ChannelListContainer.jsx
+++ src/components/ChannelListContainer/ChannelListContainer.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { ChannelList, useChatContext } from "stream-chat-react";
 import Cookies from "universal-cookie";
+import CompanyHeader from "./components/companyHeader/CompanyHeader";
 import SideBar from "./components/sidebar/SideBar";
 
 const ChannelListContainer = () => {
   const x = useChatContext();
   return (
-    <div>
-      <ChannelList>
-        <SideBar />
-      </ChannelList>
-    </div>
+    <>
+      <SideBar />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+      </div>
+    </>
   );
 };
 export default ChannelListContainer;
